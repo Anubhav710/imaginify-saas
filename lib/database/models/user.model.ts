7320a343d094c5d4c1bb550891b4be7413ea0e87
anubhav710 <anubhavagrawal710@gmail.com>
2025-07-02 11:36:31 +0530

    updated user model

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -4,17 +4,14 @@ const UserSchema = new Schema({
   clerkId: {
     type: String,
     required: true,
-    unique: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true,
   },
   username: {
     type: String,
     required: false,
-    unique: true,
   },
   photo: {
     type: String,
